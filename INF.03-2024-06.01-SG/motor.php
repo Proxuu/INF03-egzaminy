751327fb5f0967734d03669e229e8f16520b82c5
Proxuu <kacper2019114@gmail.com>
2025-01-14 16:52:35 +0100

    kwerendy i html

---
+++ INF.03-2024-06.01-SG/motor.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motocykle</title>
+    <link rel="stylesheet" type="css/text" href="styl.css">
+</head>
+<body>
+    <img src="motor.png" alt="motocykl">
+    
+    <header>
+        <div id="baner">
+            <h1>Motocykle - moja pasja</h1>
+        </div>
+    </header>
+    <main>
+        <div id="lewy">
+            <h2>Gdzie pojechać?</h2>
+            <!-- Skrypt 1-->
+        </div>
+        
+        <div id="prawy1">
+            <h2>Co kupić?</h2>
+
+            <ol>
+                <li>Honda CBR125R</li>
+                <li>Yamaha YBR125</li>
+                <li>Honda VFR800i</li>
+                <li>Honda CBR1100XX</li>
+                <li>BMW R1200GS LC</li>
+            </ol>
+        </div>
+
+        <div id="prawy2">
+            <h2>Statystyki</h2>
+            <p>Wpisanych wycieczek: <!--skrypt 2--></p>
+            <p>Użytkowników forum: 200</p>
+            <p>Przesłanych zdjęć: 1300</p>
+        </div>
+    </main>
+    <footer>
+        <div id="stopka">
+            <p>Stronę wykonał: 00000000</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
